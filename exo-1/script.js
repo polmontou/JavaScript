6124eeab871e9788b19534039eaa90a2971f3a9b
Montoussy Paul <paul.montoussy@le-campus-numerique.fr>
2025-04-24 14:22:12 +0200

    exo 1 et 2

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,3 +1,8 @@
 
 // you can write js here
 console.log('hello from file');
+var kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+let celsius = kelvins - 273;
+let fahrenheits = Math.floor(celsius*(9/5)+32); //formule pour passer des degrés celsius aux fahrenheits, le Math.floor() permet d'obtenir le + grand entier < au résultat trouvé par le calcul
+
+console.log(`${kelvins} degrés kelvins donnent ${celsius} degrés celsius et ${fahrenheits} degrés fahrenheit.`)
--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,24 @@
 // you can write js here
 console.log('exo-2');
+const myDate = new Date();
+const messageSemaine = "Pas encore!";
+const messageWeekEnd = "Profite, ça passe vite";
+let jourActuel;
+let heureActuelle;
+let testWE;
+let isTesting = false;
+
+if (isTesting) {
+    jourActuel = parseInt(prompt("Choix jour : 0 - Dimanche // 6 - Samedi","number"));
+    heureActuelle = parseInt(prompt("Choix heure","number"));
+    //jourActuel === 6 || jourActuel === 0 ? console.log(messageWeekEnd) : console.log(messageSemaine);
+    //(jourActuel === 5 && heureActuelle >= 17) || jourActuel === 6 || jourActuel === 0 ? console.log(messageWeekEnd) : console.log(messageSemaine);
+    testWE = ((jourActuel === 1 && heureActuelle < 9) || (jourActuel === 5 && heureActuelle <= 17) || jourActuel === 6 || jourActuel === 0);
+    testWE ? console.log(messageWeekEnd) : console.log(messageSemaine);      
+} else {
+    jourActuel = myDate.getDay();
+    heureActuelle = myDate.getHours();
+    //jourActuel === 6 || jourActuel === 0 ? console.log(messageWeekEnd) : console.log(messageSemaine);
+    //(jourActuel === 5 && heureActuelle >= 17) || jourActuel === 6 || jourActuel === 0 ? console.log(messageWeekEnd) : console.log(messageSemaine);
+    (jourActuel === 1 && heureActuelle < 9) || (jourActuel === 5 && heureActuelle <= 17) || jourActuel === 6 || jourActuel === 0 ? console.log(messageWeekEnd) : console.log(messageSemaine);      
+}
