5af14133ad8a49b875dd421af7a41e50f2f2bcbc
Montoussy Paul <paul.montoussy@le-campus-numerique.fr>
2025-04-25 13:42:51 +0200

    modif exo 5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,35 @@
 // you can write js here
 
 console.log('exo-5');
+let input = prompt("Votre phrase :","Strings only");
+const vowels = ["a","e","i","o","u","y"];
+let resultArray = [];
+
+for (letter of input) {
+    if (vowels.includes(letter.toLowerCase())) {
+        resultArray.push(letter.toLowerCase());
+    }
+}
+
+console.log(resultArray.join("").toUpperCase());
+
+/*OU SINON :
+
+resultArray = [];
+let inputSplit = input.split("");
+
+resultArray = inputSplit.map(letter => {
+        if (vowels.includes(letter.toLowerCase())) {
+            return letter.toLowerCase();
+        }
+    })    
+console.log(resultArray.join("").toUpperCase());*/
+
+/*OU SINON : 
+resultArray = [];
+let inputSplit = input.split("");
+
+resultArray = inputSplit.filter(letter => {
+    return vowels.includes(letter.toLowerCase()); 
+    })    
+console.log(resultArray.join("").toUpperCase());*/
