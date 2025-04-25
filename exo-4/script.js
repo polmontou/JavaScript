6c44252f3b43e3615cde99f0026a8458dfda47c5
Montoussy Paul <paul.montoussy@le-campus-numerique.fr>
2025-04-25 10:20:45 +0200

    exo4

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,18 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+//1
+secretMessage.pop();
+//2
+secretMessage.push('to','program');
+//3
+secretMessage[secretMessage.indexOf('easily')] = 'right';
+//4
+secretMessage = secretMessage.slice(1);
+//5
+secretMessage.splice(0,0,'Programming');
+//6
+secretMessage.splice(5,5,'know');
+//7
+console.log(secretMessage.join(' '));
